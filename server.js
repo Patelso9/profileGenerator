bd25f7f312da6d0721c88e64ad1530959603225b
Princess <sonali.patel.pkg@gmail.com>
2021-06-03 16:57:52 -0400

    update server.js

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ function appMenu() {
         message: 'What is your ID?'
       },
       {
+        // reccomended to add validate property for email
         type: 'input',
         name: 'managerEmail',
         message: 'What is your Email?'
@@ -91,7 +92,7 @@ function appMenu() {
       {
         type: 'input',
         name: 'engineerEmail',
-        message: 'What is your Email?'
+        message: 'What is the intern email?'
       },
       {
         type: 'input',
@@ -99,37 +100,56 @@ function appMenu() {
         message: 'What is the engineer GitHUb username?'
       },
     ]).then(answers => {
-      //
+      
+      const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
+      teamMembers.push(engineer);
+      idArray.push(answers.engineerId);
+    
       // YOUR CODE HERE
       // 1. CREATE A VARIABLE TO STORE THE ENGINEER OBJECT INSTANTIATED WITH THE ENGINEER CLASS, PASSING ANSWERS PROPERTIES AS INPUT AURGUMENTS 
       //    TO THE ENGINEER CLASS CONSTRUCTOR
       // 2. ADD (PUSH) THE ENGINEER VARIABLE TO the teamMembers ARRAY
       // 3. ADD (PUSH) THE ENGINERR ID TO THE idArray ARRAY
-      // 
-
-      const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
-      teamMembers.push(engineer);
-      idArray.push(answers.engineerId);
- 
+      
+      
       createTeam();
     });
   }
 
   function addIntern() {
     inquirer.prompt([
-      //
-      // YOUR CODE HERE
-      // CREATE OBJECTS OF QUESTIONS FOR ENGINEER
-      //
+       //questions
+       {
+        type: 'input',
+        name: 'internName',
+        message: 'What is the intern name?'
+      },
+      {
+        type: 'input',
+        name: 'internId',
+        message: 'What is the intern ID?'
+      },
+      {
+        type: 'input',
+        name: 'internEmail',
+        message: 'What is the intern email?'
+      },
+      {
+        type: 'input',
+        name: 'internSchool',
+        message: 'What school does the intern attend?'
+      },
     ]).then(answers => {
-      //
+      const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internGithub);
+      teamMembers.push(intern);
+      idArray.push(answers.internId);
+      
       // YOUR CODE HERE
       // 1. CREATE A VARIABLE TO STORE THE INTERN OBJECT INSTANTIATED WITH THE INTERN CLASS, PASSING ANSWERS PROPERTIES AS INPUT AURGUMENTS 
       //    TO THE INTERN CLASS CONSTRUCTOR
       // 2. ADD (PUSH) THE INTERN VARIABLE TO the teamMembers ARRAY
       // 3. ADD (PUSH) THE INTERN ID TO THE idArray ARRAY
-      // 
-
+     
       createTeam();
     });
   }
